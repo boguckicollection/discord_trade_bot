5294fb2e9ded46bf750fa2d8f538107185b6b4e9
boguckicollection <boguckicollection@gmail.com>
2025-06-24 11:01:03 +0200

    Load environment variables

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from discord.ext import commands
 from discord.ui import Modal, TextInput, View, Button
 from datetime import datetime, timedelta
 import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
